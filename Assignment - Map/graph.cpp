aa246d945a6d77efc219d1637b8bee5a81694f9e
s181426 <s181426@ADL-A5A272>
2018-08-09 14:59:48 +0930

    2d vector node structure working

--- Assignment - Map/graph.cpp
+++ Assignment - Map/graph.cpp
@@ -12,6 +12,8 @@ graph::graph()
 	node_size = 22;
 	graph_width = 20;
 	graph_height = 20;
+	start_loc.x = 20;
+	start_loc.y = 20;
 }
 
 graph::~graph()
@@ -20,19 +22,38 @@ graph::~graph()
 
 void graph::createGraph()
 {
+	/*
 	node_start = new node(50, 50);
 	node* current_node = node_start;
 	for (int i = 0; i < graph_width; i++) {
 		current_node->setRight(new node(i*node_size + 50, 50));
 		current_node = current_node->getRight();
+	}*/
+	
+	for (int i = 0; i < graph_width; i++) {
+		std::vector<node> v;
+		nodes.push_back(v);
+		for (int o = 0; o < graph_width; o++) {
+			nodes[i].push_back(node(((node_size * o) + start_loc.x), (node_size * i) + start_loc.y));
+		}
 	}
+
+
 }
 
 void graph::drawGraph(aie::Renderer2D * ren)
 {
+	/*
 	node* current_node = node_start;
 	for (int i = 0; i < graph_width; i++) {
 		current_node->drawNode(ren);
 		current_node = current_node->getRight();
+	}*/
+	for (int i = 0; i < graph_width; i++) {
+		for (int o = 0; o < graph_width -1; o++) {
+			nodes[i][o].drawNode(ren);
+		}
 	}
+
+
 }
--- Assignment - Map/graph.h
+++ Assignment - Map/graph.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "node.h"
+#include <vector>
 #include "Renderer2D.h"
 class graph
 {
@@ -13,8 +14,11 @@ protected:
 	float node_size;
 	int graph_height;
 	int graph_width;
+	Vector2 start_loc;
+
 	float node_diagonal_size;
 	node* node_start;
 
+	std::vector< std::vector<node> > nodes;
 };
 
